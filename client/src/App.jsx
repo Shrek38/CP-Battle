5b6c73144a17e8b2d54757350c0da92618da8cc9
Shrek38 <vedaryaiitr@gmail.com>
2026-05-26 15:41:56 +0530

    Project scaffold + React fundamentals preview

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import BattleRoomPreview from './BattleRoomPreview'
+
+function App() {
+  return (
+    <div>
+      <h1>DSA Battle — Day 1 Preview</h1>
+      <BattleRoomPreview />
+    </div>
+  )
+}
+
+export default App
