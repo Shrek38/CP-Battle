c77f3269603d9bf224ac7a342e97e62059477a39
Shrek38 <vedaryaiitr@gmail.com>
2026-06-02 00:23:32 +0530

    Fix backend URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -5,7 +5,7 @@
 
 import { io } from 'socket.io-client'
 
-const SERVER_URL = 'https://cp-battle-zwoh.onrender.com'
+const SERVER_URL = 'https://cp-battle-1.onrender.com'
 
 // autoConnect: false means the socket won't connect until we call socket.connect()
 // This gives us control over when the connection opens
